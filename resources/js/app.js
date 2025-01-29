a0ce686bfd406086427a79f780308d147718b2d2
John Arby Arceo <johnarbyarceo@gmail.com>
2025-01-30 01:51:13 +0800

    added vue and inertiajs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-import './bootstrap';
+import "./bootstrap";
+import "../css/app.css";
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { createBootstrap } from "bootstrap-vue-next";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./vue/Pages/**/*.vue", { eager: true });
+        console.log(pages);
+
+        return pages[`./vue/Pages/${name}.vue`];
+    },
+    title: (title) => (title ? `${title} - App` : "App"),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(createBootstrap())
+            .mount(el);
+    },
+});
